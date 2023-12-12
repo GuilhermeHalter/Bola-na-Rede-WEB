01c5256c037fb1bd9af01a8fd6e11c869f7f092b
GuilhermeHalter <guisam.halter@gmail.com>
2023-12-12 10:07:44 -0300

    tentando arrumar css

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,8 +1,38 @@
 <template>
-    <main>
-        bem vindo ao site Bola na Rede
+    <main class="home">
+      <div class="content">
+        <h1 class="title">Bola na Rede</h1>
+        <p class="intro-text">
+          Explore o emocionante mundo do futebol conosco. Notícias, camisas exclusivas e tudo que você precisa para viver a paixão pelo esporte mais popular do mundo.
+        </p>
+      </div>
     </main>
-</template>
-
-<style>
-</style>
+  </template>
+  
+  <style scoped>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fff;
+    color: #000; 
+  }
+  
+  .content {
+    text-align: center;
+  }
+  
+  .title {
+    font-size: 3em;
+    margin-bottom: 20px;
+    font-weight: bold;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  
+  .intro-text {
+    font-size: 1.3em;
+  }
+  </style>
+  
